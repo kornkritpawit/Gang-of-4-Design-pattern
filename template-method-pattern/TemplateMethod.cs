294367d733712ef35ccfcae7ee4a12d108e76889
Kritpawit Soongswang <kritpawitsoongswang@Kritpawits-MacBook-Pro.local>
2024-03-27 15:35:46 +0700

    commit

--- template-method-pattern/TemplateMethod.cs
+++ template-method-pattern/TemplateMethod.cs
@@ -9,23 +9,19 @@ namespace template_method_pattern
     public abstract class TemplateMethod
     {
         private readonly AuditTrail _auditTrail;
-
         public TemplateMethod()
         {
             _auditTrail = new AuditTrail();
         }
-
         public TemplateMethod(AuditTrail auditTrail)
         {
             _auditTrail = auditTrail;
         }
-
         public void Execute() {
             _auditTrail.Record();
             DoExecute();
             _auditTrail.Stop();
         }
-
         protected abstract void DoExecute();
     }
 }
