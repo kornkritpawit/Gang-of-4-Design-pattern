0dec87e2ebfa3842c3e6d421946c745799e1ff9a
Kritpawit Soongswang <kritpawit.s@gmail.com>
2024-04-09 13:59:23 +0700

    commit

--- flyweight-pattern/Program.cs
+++ flyweight-pattern/Program.cs
@@ -5,18 +5,27 @@ namespace pattern_flyweight
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // var service = new PointService();
-            // foreach (var point in service.GetPoints()) {
-            //     point.Draw();
-            // }
-
-            var factory = new PointIconFactory();
-            var service = new PointServiceFw(factory);
+            var service = new PointService();
             foreach (var point in service.GetPoints()) {
                 point.Draw();
             }
         }
+        
+        // static void Main(string[] args)
+        // {
+        //     // var service = new PointService();
+        //     // foreach (var point in service.GetPoints()) {
+        //     //     point.Draw();
+        //     // }
+
+        //     var factory = new PointIconFactory();
+        //     var service = new PointServiceFw(factory);
+        //     foreach (var point in service.GetPoints()) {
+        //         point.Draw();
+        //     }
+        // }
     }
 }
