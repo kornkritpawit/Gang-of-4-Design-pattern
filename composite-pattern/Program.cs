ee49b60b31e921f70cf288f87fd4b2f48a0cceb7
Kritpawit Soongswang <kritpawit.s@gmail.com>
2024-04-10 10:13:18 +0700

    commit

--- composite-pattern/Program.cs
+++ composite-pattern/Program.cs
@@ -6,39 +6,61 @@ namespace pattern_composite
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            // var shape1 = new Shape();
-            // var shape2 = new Shape();
-
-            var shape1 = new ShapeComposite();
-            var shape2 = new ShapeComposite();
-
-            // var group1 = new Group();
-            // var group1 = new GroupCompositeBad();
-            var group1 = new GroupComposite();
+            var shape1 = new Shape();
+            var shape2 = new Shape();
+            var group1 = new Group();
             group1.Add(shape1);
             group1.Add(shape2);
 
-            var shape3 = new ShapeComposite();
-            var shape4 = new ShapeComposite();
-            // var group2 = new Group();
-            // var group2 = new GroupCompositeBad();
-            var group2 = new GroupComposite();
-            group1.Add(shape3);
-            group1.Add(shape4);
+            var shape3 = new Shape();
+            var shape4 = new Shape();
+            var group2 = new Group();
 
-            // var topGroup = new Group(); //เอา Group ใส่ group ไม่ได้
-            // topGroup.Add(group1);
-            // topGroup.Add(group2); //
+            group2.Add(shape3);
+            group2.Add(shape4);
 
-            // var topGroup = new GroupCompositeBad();
-            var topGroup = new GroupComposite();
+            var topGroup = new Group(); //เอา Group ใส่ group ไม่ได้
             topGroup.Add(group1);
-            topGroup.Add(group2);
-            topGroup.Render();
-            topGroup.Move();
-
+            topGroup.Add(group2); //
         }
+
+    //     static void Main(string[] args)
+    //     {
+    //         // var shape1 = new Shape();
+    //         // var shape2 = new Shape();
+
+    //         var shape1 = new ShapeComposite();
+    //         var shape2 = new ShapeComposite();
+
+    //         // var group1 = new Group();
+    //         // var group1 = new GroupCompositeBad();
+    //         var group1 = new GroupComposite();
+    //         group1.Add(shape1);
+    //         group1.Add(shape2);
+
+    //         var shape3 = new ShapeComposite();
+    //         var shape4 = new ShapeComposite();
+    //         // var group2 = new Group();
+    //         // var group2 = new GroupCompositeBad();
+    //         var group2 = new GroupComposite();
+    //         group1.Add(shape3);
+    //         group1.Add(shape4);
+
+    //         // var topGroup = new Group(); //เอา Group ใส่ group ไม่ได้
+    //         // topGroup.Add(group1);
+    //         // topGroup.Add(group2); //
+
+    //         // var topGroup = new GroupCompositeBad();
+    //         var topGroup = new GroupComposite();
+    //         topGroup.Add(group1);
+    //         topGroup.Add(group2);
+    //         topGroup.Render();
+    //         topGroup.Move();
+
+    //     }
+
     }
 }
