cdd7a8fc145c4fcb5d29312a26439ef1a40e52a1
Kritpawit Soongswang <kritpawit.s@gmail.com>
2024-04-16 14:49:14 +0700

    commit

--- facade-pattern/NotificationService.cs
+++ facade-pattern/NotificationService.cs
@@ -2,13 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using pattern_facade;
 
 namespace facade_pattern
 {
     public class NotificationService
     {
-        public void Send(string message, string target) {
-            
+        public void Send(string message, string target)
+        {
+            var server = new NotificationServer();
+            var connection = server.Connection("ip");
+            var authToken = server.Authenticate("appId", "key");
+            server.Send(authToken, new Message(message), "target");
+            connection.Disconnect();
         }
     }
 }
--- facade-pattern/Program.cs
+++ facade-pattern/Program.cs
@@ -5,16 +5,21 @@ namespace pattern_facade
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            var message = new Message("this is sending message");
-            var server = new NotificationServer();
-            var connection = server.Connection("ip");
-            var authToken = server.Authenticate("appId", "key");
-            server.Send(authToken, message, "target");
-            connection.Disconnect();
-            // ปัญหาของแบบข้างบน ใน main คือ โค้ดข้างบนมีความเยอะมาก ต้องสร้างหลาย Object
-            // ทำหลายอย่าง
-        }
+        // static void Main(string[] args)
+        // {
+        //     var message = new Message("this is sending message");
+        //     var server = new NotificationServer();
+        //     var connection = server.Connection("ip");
+        //     var authToken = server.Authenticate("appId", "key");
+        //     server.Send(authToken, message, "target");
+        //     connection.Disconnect();
+        //     // ปัญหาของแบบข้างบน ใน main คือ โค้ดข้างบนมีความเยอะมาก ต้องสร้างหลาย Object
+        //     // ทำหลายอย่าง
+        // }
+        static void Main(string[] args) {
+            var notificationService = new NotificationService();
+            notificationService.Send("hello", "target");
+         }
+
     }
 }
